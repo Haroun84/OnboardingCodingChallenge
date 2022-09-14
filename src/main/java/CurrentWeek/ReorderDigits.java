36940baccae75cc83f5424c6a79b7cfb73258b90
Haroun84 <mimih84@gmail.com>
2022-09-13 23:16:23 -0400

    Added comments

--- src/main/java/CurrentWeek/ReorderDigits.java
+++ src/main/java/CurrentWeek/ReorderDigits.java
@@ -14,8 +14,14 @@ public class ReorderDigits {
         System.out.println("Reorder each element of array " + Arrays.toString(arr)
                            + " in descending order: " +reorderDigitsDesc(arr));
     }
-        /*
-
+        /* Method to reorder each integer of the array in ascendant order
+           - Convert each integer into an array of string
+           - Create an array of digits with same length as string array
+           - Add each digit to the digits array
+           - Sort the digit array
+           - Construct a string with the digits of the number
+           - Add each string to an arraylist
+           - Print array
          */
     public static String reorderDigitsAsc(int[] arr) {
 
@@ -26,22 +32,26 @@ public class ReorderDigits {
             String s = Integer.toString(n);
             int[] digitArrays = new int[s.length()];
             int index = digitArrays.length - 1;
-            while (n > 0) {
-                int lastDigit = n % 10;
-                digitArrays[index] = lastDigit;
-                n /= 10;
-                index--;
-            }
-            Arrays.sort(digitArrays);
-            String m = "";
+
+                while (n > 0) {
+                    int lastDigit = n % 10;
+                    digitArrays[index] = lastDigit;
+                    n /= 10;
+                    index--;
+                }
+
+                Arrays.sort(digitArrays); // Sort array of digits(from each element of array) in ascending order
+                String m = "";
+
             for (int num : digitArrays) {
                 m += num;
             }
-            myList.add(m);
+            myList.add(m); // add each string constructed from digits of a number to list
         }
-        return myList.toString();
+        return myList.toString(); // printing final array
     }
 
+    //Method to reorder each integer of the array in descending order
     public static String reorderDigitsDesc(int[] arr) {
 
         List<String> myList = new ArrayList<>();
@@ -51,18 +61,21 @@ public class ReorderDigits {
             String s = Integer.toString(n);
             Integer[] digitArrays = new Integer[s.length()];
             int index = digitArrays.length - 1;
-            while (n > 0) {
-                int lastDigit = n % 10;
-                digitArrays[index] = lastDigit;
-                n /= 10;
-                index--;
-            }
-            Arrays.sort(digitArrays, Collections.reverseOrder());
-            String m = "";
+
+                while (n > 0) {
+                    int lastDigit = n % 10;
+                    digitArrays[index] = lastDigit;
+                    n /= 10;
+                    index--;
+                }
+
+                Arrays.sort(digitArrays, Collections.reverseOrder());//Sorting array in descending order
+                String m = "";
+
             for (int num : digitArrays) {
                 m += num;
             }
-            myList.add(m);
+            myList.add(m);// add each string constructed from digits of a number to list
         }
         return myList.toString();
     }
